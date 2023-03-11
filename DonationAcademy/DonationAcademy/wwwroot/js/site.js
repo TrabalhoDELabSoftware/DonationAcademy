7700e281b12a92258f859d00ab14f866ba753b0f
Luis Mendes <lms.kracker08@gmail.com>
2023-03-11 16:27:46 -0300

    Fase inicial do projeto

---
+++ DonationAcademy/DonationAcademy/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿var nextButton = document.querySelector(".carousel-control-next");
+var prevButton = document.querySelector(".carousel-control-prev");
+
+nextButton.addEventListener("click", function () {
+    $('#carouselStore').carousel('next');
+});
+
+prevButton.addEventListener("click", function () {
+    $('#carouselStore').carousel('prev');
+});
+
